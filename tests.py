ebae0a2fb74abd27f763ae933116e4c8f55b082e
Bruno Melo <brunousml@br.ibm.com>
2018-03-23 16:19:35 -0300

    Add test to Calculator.sum()

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+from Calculator import Calculator
+
+
+class CalculatorTest(unittest.TestCase):
+    def test_sum_with_success(self):
+        # Set Up
+        expected = 2
+        a = 1
+        b = 1
+        calculator = Calculator(a, b)
+
+        # ACT
+        actual = calculator.sum()
+
+        # Assert
+        self.assertEqual(actual, expected, msg="We expected {0} but receive {1}".format(expected, actual))
+
+
+if __name__ == '__main__':
+    unittest.main()
